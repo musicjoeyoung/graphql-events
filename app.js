484a2e9eaabad27652adfed50bb34e81de17a9f3
Joe Young <musicjoeyoung@gmail.com>
2021-04-18 11:36:51 -0400

    growing schema changes)

--- app.js
+++ app.js
@@ -25,11 +25,13 @@ app.use(
         description: String!
         price: Float!
         date: String!
+        creator: User!
     }
     type User {
       _id: ID!
       email: String!
       password: String
+      createdEvents: [Event!]
     }
     input EventInput {
         title: String!
@@ -56,9 +58,17 @@ app.use(
     rootValue: {
       events: () => {
         return Event.find()
+          .populate("creator")
           .then((events) => {
             return events.map((event) => {
-              return { ...event._doc, _id: event.id };
+              return {
+                ...event._doc,
+                _id: event.id,
+                creator: {
+                  ...event._doc.creator._doc,
+                  _id: event._doc.creator.id,
+                },
+              };
             });
           })
           .catch((err) => {
